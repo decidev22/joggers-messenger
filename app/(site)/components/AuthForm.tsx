e3f94bf4818e1c773bad45dc5ac3524fcb5e5093
decidev22 <hamento22@gmail.com>
2023-06-26 20:31:17 +1200

    Sign in screen with basic auth method set.

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Button from "@/app/components/Button";
+import Input from "@/app/components/inputs/input";
 import { useCallback, useState } from "react";
 import {
   Field,
@@ -7,6 +9,10 @@ import {
   SubmitHandler,
   useForm,
 } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+
+//icons
+import { BsGithub, BsGoogle } from "react-icons/bs";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -42,13 +48,128 @@ const AuthForm = () => {
     if (variant === "LOGIN") {
       // next auth login feat
     }
-
-    const socialAction = (action: string) => {
-      setIsLoading(true);
-      // Social Sign In
-    };
   };
-  return <div> Auth Form Works</div>;
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+    // Social Sign In
+  };
+  return (
+    <div
+      className="
+        mt-6
+        sm:mx-auto
+        sm:w-full
+        sm:max-w-md
+        "
+    >
+      <div
+        className="
+        bg-white
+        px-4
+        py-8
+        shadow
+        sm:rounded-lg
+        sm:px-10
+        "
+      >
+        <form
+          className="
+             space-y-6"
+          onSubmit={handleSubmit(onSubmit)} //handleSubmit(onSubmit) instead of {onSubmit} to pass 'data'
+        >
+          {variant === "REGISTER" && (
+            <Input
+              id="name"
+              label="Name"
+              register={register}
+              errors={errors}
+            />
+          )}
+          <Input
+            id="email"
+            label="Email"
+            type="email"
+            register={register}
+            errors={errors}
+          />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === "LOGIN" ? "Sign In" : "Register"}
+            </Button>
+          </div>
+        </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div
+              className="
+                absolute
+                inset-0
+                flex
+                items-center
+              "
+            >
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div
+              className="
+                relative
+                flex
+                justify-center
+                text-sm
+                "
+            >
+              <span className="bg-white px-2 text-gray-500">
+                OR continue with
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction("github")}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction("google")}
+            />
+          </div>
+        </div>
+
+        <div
+          className="
+            flex
+            gap-2
+            justify-center
+            text-sm
+            mt-6
+            px-2
+            text-gray-500
+        "
+        >
+          <div>
+            {variant === "LOGIN"
+              ? "New to JoggersMessenger?"
+              : "Already have an account?"}
+          </div>
+          <div
+            onClick={toggleVariant}
+            className="underline cursor-pointer"
+          >
+            {variant === "LOGIN" ? "Create an account" : "Login"}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AuthForm;
